ae19e37f4d2b129100979a7b356b8f6d96851221
Alex-Yu <alexyuzhu@gmail.com>
2022-01-07 03:43:14 +0800

    feat: Encode respondents with higher edu as 1

--- helper.py
+++ helper.py
@@ -47,3 +47,48 @@ def get_description(schema, feature_name):
         return "WARNING: Check your spelling as no feature called {}".format(feature_name)
         
     return desc
+
+
+def method_suggested_count(df, plot=True):
+    """[summary]
+
+    Args:
+        df (pd.DataFrame): dataset of the developers survey
+        plot (bool, optional): [Whether or not plot the result]. Defaults to True.
+        
+    Returns:
+        None
+    """
+    couEduSplit = df[df.CousinEducation.notna()].CousinEducation.str.split('; ', n=-1, expand=True)
+    series_lst = []
+    for i in range(4):
+        series_lst.append(couEduSplit[i].value_counts().sort_index())
+    cc = pd.concat(series_lst, axis=1).sum(axis=1)
+    cc_relative = (cc/cc.sum()).sort_values()
+    
+    print(cc_relative.sort_values(ascending=False))
+    
+    if plot:
+        plt.figure(figsize=(8, 6))
+        plt.barh(cc_relative.index, cc_relative.values, height=.6)
+
+        plt.title("Method of Educating Suggested", fontsize=16)
+        plt.ylabel("Methods", fontweight='bold', fontsize=12)
+        plt.xlabel('Relative Freq.', fontweight='bold', fontsize=12, labelpad=12);
+        
+        
+def higher_ed_mapping(df, edu_levels):
+    """Encode education level in given `edu_levels` list as 1 or 0
+
+    Args:
+        df (pd.DataFrame): dataset of the developers survey
+        edu_levels ([list]): education level needs to be encoded as 1
+
+    Returns:
+        df (pd.DataFrame): A new dataframe with `higher_ed` column added
+        prop (float): percentage of respondents with higher edu level
+    """
+    df['higher_ed'] = df.FormalEducation.isin(edu_levels).astype('int')
+    prop = df['higher_ed'].mean()
+    return df, prop
+    
